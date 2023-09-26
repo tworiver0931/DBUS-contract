ff9a71f0deef8c648fc6c684d4f50b7e6bc4db3d
tworiver0931 <tworiver0931@gmail.com>
2023-09-27 03:03:23 +0900

    calQF

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -17,6 +17,7 @@ contract FundRegistry {
         address payee; // 토큰 수취인 주소
         uint256 threshold; // 펀딩 임계 토큰량
         uint256 totalAmount; // 현재 펀딩된 토큰량
+        bool isEnd; // 펀딩이 끝났는지지
         // 펀딩(노선)의 메타데이터도 추가해야함
     }
 
@@ -28,8 +29,24 @@ contract FundRegistry {
 
     mapping(uint96 => Fund) public funds;
 
-    event FundCreated(uint96 indexed id, address owner, address payee, uint256 indexed threshold, uint256 indexed amount, uint time);
-    event FundUpdated(uint96 indexed id, address owner, address payee, uint256 indexed threshold, uint256 indexed amount, uint time);
+    event FundCreated(
+        uint96 indexed id, 
+        address owner, 
+        address payee, 
+        uint256 indexed threshold, 
+        uint256 indexed amount, 
+        bool isEnd, 
+        uint time
+    );
+    event FundUpdated(
+        uint96 indexed id, 
+        address owner, 
+        address payee, 
+        uint256 indexed threshold, 
+        uint256 indexed amount, 
+        bool isEnd, 
+        uint time
+    );
 
     function createFund(
         address _owner,
@@ -37,8 +54,17 @@ contract FundRegistry {
         uint256 _threshold
     ) external {
         uint96 _id = fundCount;
-        funds[_id] = Fund(_id, _owner, uint48(block.timestamp), uint48(block.timestamp), _payee, _threshold, 0);
-        emit FundCreated(_id, _owner, _payee, _threshold, 0, block.timestamp);
+        funds[_id] = Fund(
+            _id, 
+            _owner, 
+            uint48(block.timestamp), 
+            uint48(block.timestamp), 
+            _payee, 
+            _threshold, 
+            0, 
+            false
+        );
+        emit FundCreated(_id, _owner, _payee, _threshold, 0, false, block.timestamp);
         fundCount += 1;
     }
 
@@ -50,8 +76,17 @@ contract FundRegistry {
     ) external {
         Fund memory _fund = funds[_id];
         require(msg.sender == _fund.owner, "Fund Update not authorized");
-        funds[_id] = Fund(_id, _owner, _fund.createdAt, uint48(block.timestamp), _payee, _threshold, _fund.totalAmount);
-        emit FundUpdated(_id, _owner, _payee, _threshold, _fund.totalAmount, block.timestamp);
+        funds[_id] = Fund(
+            _id, 
+            _owner, 
+            _fund.createdAt, 
+            uint48(block.timestamp), 
+            _payee, 
+            _threshold, 
+            _fund.totalAmount,
+            _fund.isEnd
+        );
+        emit FundUpdated(_id, _owner, _payee, _threshold, _fund.totalAmount, _fund.isEnd, block.timestamp);
     }
     
     function getAllFunds() external view returns (Fund[] memory){
@@ -76,7 +111,14 @@ contract FundRegistry {
     function donate(
         Donation calldata _donation
     ) external payable {
-        // QF
-        // 검증
+        // QF 계산
+        Fund[] memory updatedFundList = calculateQF(_donation.fundId, _donation.amount);
+
+        // 업데이트, 검증
+    }
+
+    function calculateQF(uint96 _fundId, uint256 _amount) internal view returns (Fund[] memory){
+        // 모든 isEnd=false 펀드 정보 불러오기 (펀드 struct에 참여한 유저 정보도 저장)
+        // QF 계산 후 업데이트된 펀드 정보 반환
     }
 }
